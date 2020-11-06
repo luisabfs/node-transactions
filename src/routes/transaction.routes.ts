a623b9c1ecb08fa6b884ab9a1b6a9ad3e0632710
Luisa Barros <qlullyq@gmail.com>
2020-11-06 08:04:36 -0300

    feat: add import route

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-import CreateCategoryService from '../services/CreateCategoryService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
@@ -23,14 +27,11 @@ transactionRouter.post('/', async (request, response) => {
   const { title, value, type, category: categoryTitle } = request.body;
   const createTransaction = new CreateTransactionService();
 
-  const createCategory = new CreateCategoryService();
-  const category = await createCategory.execute({ title: categoryTitle });
-
   const transaction = await createTransaction.execute({
     title,
     value,
     type,
-    category,
+    categoryTitle,
   });
 
   return response.json(transaction);
@@ -44,5 +45,19 @@ transactionRouter.delete('/:id', async (request, response) => {
 
   return response.send();
 });
+
+transactionRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactions = new ImportTransactionsService();
+
+    const transactions = await importTransactions.execute(
+      request.file.filename,
+    );
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionRouter;
