0889e45c1296a002c6716ab146dd57020bccbd69
Luisa Barros <qlullyq@gmail.com>
2020-11-06 08:05:36 -0300

    feat: add custom error handler

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,39 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
+
+import AppError from './errors/AppError';
 
 import createConnection from './database';
 
 createConnection();
 const app = express();
 
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error.',
+    });
+  },
+);
+
 app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('✨️ Server started at port 3333! ✨️ ');
 });
 
 export default app;
