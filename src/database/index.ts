e6e3d725d83501516de71c18660e89e38ad4065a
Luisa Barros <qlullyq@gmail.com>
2020-11-09 17:11:11 -0300

    fix: change database name for tests

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'node_transactions_tests'
+          ? 'gostack_desafio06_tests'
           : defaultOptions.database,
     }),
   );
