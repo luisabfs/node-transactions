45bc6e75c21470bc26cba1cc51237bd70df64f90
Luisa Barros <qlullyq@gmail.com>
2020-11-09 22:38:57 -0300

    chore: add cors as express middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'express-async-errors';
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
 
@@ -13,6 +14,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use(
